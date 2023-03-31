451eb56bc8361a6910f4b28c5bc5ad717751bef0
Jelly <pbp980822@nate.com>
2023-03-31 17:17:43 +0900

    draw map

--- 230331.cpp
+++ 230331.cpp
@@ -22,7 +22,15 @@ int main()
 	{
 		for (int X = 0; X < 10; X++)
 		{
-			cout << Map[Y][X];
+			if (Map[Y][X] == 1)
+			{
+				cout << "#";
+			}
+			else if (Map[Y][X] == 0)
+			{
+				cout << ' ';
+			}
+			
 		}
 		cout << '\n';
 	}
